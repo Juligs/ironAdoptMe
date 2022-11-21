b2a06f95aae17236c1326f67e7c0fd33bacd2462
cbrioneslopez <cbrioneslopez96@gmail.com>
2022-11-21 17:17:02 +0100

    Co-authored-by: Juliana <Juligs@users.noreply.github.com>

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -4,19 +4,26 @@ const fileUploader = require('../config/cloudinary.config');
 const { isLoggedIn, checkRoles } = require('./../middleware/route-guard');
 const { create } = require("hbs");
 
-router.get("/map", (req, res, next) => res.render("event/event-user"))
+router.get("/map", (req, res, next) => {
+    Event
+        .find()
+        .then(foundEvents => res.render("event/event-user", { foundEvents }))
+        .catch(err => console.log(err))
 
+
+})
 router.get('/create', isLoggedIn, checkRoles("SHELTER", "ADMIN"), (req, res, next) => {
     res.render('event/create-event')
 
 })
-router.post('/create', isLoggedIn, checkRoles("SHELTER", "ADMIN"), (req, res, next) => {
-    const { title, description, date, address, image } = req.body
+router.post('/create', isLoggedIn, checkRoles("SHELTER", "ADMIN"), fileUploader.single("image"), (req, res, next) => {
+    const { title, description, date, address } = req.body
     Event
-        .create({ title, description, date, address, image })
+        .create({ title, description, date, address, image: req.file.path })
         .then(() => res.redirect('/event/map'))
         .catch(err => console.log(err))
 })
+
 
 module.exports = router;
 
