eebf10b351eae8ace5eaf2251fbee485b7eab63d
cbrioneslopez <cbrioneslopez96@gmail.com>
2022-11-24 21:03:40 +0100

    Added location based filters

--- routes/pets.routes.js
+++ routes/pets.routes.js
@@ -3,6 +3,8 @@ const Pet = require("./../models/Pet.model")
 const User = require("./../models/User.model")
 const fileUploader = require('../config/cloudinary.config');
 const { isLoggedIn, checkRoles } = require('./../middleware/route-guard');
+const maps = require("./../services/map-api")
+const mapsApi = new maps()
 
 router.get("/list", isLoggedIn, (req, res, next) => {
 
@@ -16,13 +18,13 @@ router.get("/create", isLoggedIn, checkRoles("SHELTER", "ADMIN"), (req, res, nex
 
 router.post("/create", isLoggedIn, checkRoles("SHELTER", "ADMIN"), fileUploader.single("petImg"), (req, res, next) => {
 
-    const { name, age, breed, description } = req.body
+    const { name, age, breed, description, species } = req.body
     const { path: image } = req.file
     const { _id: shelterBy } = req.session.currentUser
     const { location } = req.session.currentUser
 
     Pet
-        .create({ name, age, breed, location, description, image, shelterBy })
+        .create({ name, age, breed, location, species, description, image, shelterBy })
         .then(createdPet => User.findByIdAndUpdate(shelterBy, { $push: { pets: createdPet._id } }))
         .then(() => res.redirect("/pets/list"))
         .catch(err => console.log(err))
@@ -71,5 +73,39 @@ router.post("/:idPet/delete", isLoggedIn, checkRoles("SHELTER", "ADMIN"), (req, 
         .then(() => res.redirect("/pets/list"))
         .catch(err => console.log(err))
 })
+
+router.get("/filter", isLoggedIn, async (req, res) => {
+
+    const { age, breed, species, address } = req.query
+    const query = {}
+
+
+    if (age) query.age = age
+    if (breed) query.breed = breed
+    if (species) query.species = species
+    if (address) {
+        const geoCodedAddress = await mapsApi.geocodeAddress(address)
+        const { lat, lng } = geoCodedAddress
+        query.location = {
+            $near: {
+                $maxDistance: 20000,
+                $geometry: {
+                    type: "Point",
+                    coordinates: [lng, lat]
+                }
+            }
+        }
+    }
+
+
+
+    Pet
+        .find(query)
+        .then(pets => res.render("pets/pets-list", { pets }))
+        .catch(err => next(err))
+
+
+})
+
 
 module.exports = router
